237434a6380ac83e0493b233a6fa4a28037b57fc
Mykhailo <rosmisha@gmail.com>
2023-03-26 20:54:38 +0300

    Add exercise_4

---
+++ Home_task_1/exercise_4/exercise_4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace exercise_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Tensor test = new Tensor(4);
+            int[] where = { 2, 2, 1 };
+            test.InitializationTensor(3, 5, 2);
+            Console.WriteLine(test.ToString());
+            test.SetValue(9, 1, 2, 0);
+            Console.WriteLine(test.GetValue(where));
+            Console.WriteLine(test.GetValue( 0, 3, 0 ));
+            test[1, 1, 0] = 44;
+            Console.WriteLine("Print element: {0}", test[1, 1, 0]);
+
+            Console.WriteLine(test.ToString());
+
+            Tensor test2 = new Tensor(1);
+            test2.InitializationTensor(1);
+            Console.WriteLine(test2.ToString());
+            test2.SetValue(9, 0);
+            Console.WriteLine(test2.ToString());
+
+            Tensor test3 = new Tensor(2);
+            test3.InitializationTensor(88);
+            test3.SetValue(88438, 1);
+            test3[22] = 5874;
+            Console.WriteLine(test3.ToString());
+            Tensor test4 = new Tensor(-1);
+        }
+    }
+}
---
+++ Home_task_1/exercise_4/exercise_4/Tensor.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Text;
+
+namespace exercise_4
+{
+    internal class Tensor
+    {
+        private int[] _indexes;
+        private int[] _tensor;
+        private int[] _multipliers;
+
+        public Tensor(int dimension)
+        {
+            if (dimension < 0)
+            {
+                throw new ArgumentOutOfRangeException("dimension", dimension, "Dimension can't be less than 0");
+            }
+            if (dimension == 1)
+            {
+                this._tensor = new int[dimension];
+            }
+            this._indexes = new int[dimension-1];
+        }
+
+        public void InitializationTensor(params int[] indexes)
+        {
+            if (indexes == null)
+            {
+                throw new ArgumentNullException(nameof(indexes));
+            }
+            int length = indexes.Length;
+            this._indexes = indexes;
+            this._tensor = new int[length];
+            this._multipliers = GetMultipliers();
+        }
+
+        public void SetValue(int value, params int[] index)
+        {
+            if (index.Length != _indexes.Length)
+            {
+                throw new ArgumentOutOfRangeException("index", index, "Incorrect element address indices are specified");
+            }
+            try
+            {
+                int index_in_tensor = GetIndexTensor(index);
+                this._tensor[index_in_tensor] = value;
+            }
+            catch (Exception e)
+            {
+                throw new ArithmeticException(e.Message);
+            }
+        }
+
+        public int GetValue(params int[] index)
+        {
+            if (index.Length != _indexes.Length)
+            {
+                throw new ArgumentOutOfRangeException("index", index, "Incorrect element address indices are specified");
+            }
+            try
+            {
+                int index_in_tensor = GetIndexTensor(index);
+                int value = this._tensor[index_in_tensor];
+                return value;
+            }
+            catch (Exception e)
+            {
+                throw new ArithmeticException(e.Message);
+            }
+        }
+
+        public int this[params int[] index]
+        {
+            get { return GetValue(index); }
+            set { SetValue(value, index); }
+        }
+
+        private int GetIndexTensor(int[] input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
+            int indexes = 0;
+            int[] mult = _multipliers;
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                indexes += input[i] * mult[i];
+            }
+            return indexes;
+        }
+
+        private int[] GetMultipliers()
+        {
+            int[] multipliers = new int[_indexes.Length];
+            multipliers[_indexes.Length - 1] = 1;
+            for (int i = _indexes.Length - 2; i >= 0; i--)
+            {
+                multipliers[i] = _indexes[i + 1] * multipliers[i + 1];
+            }
+            return multipliers;
+        }
+
+        //Matrix output to the console
+        public override string ToString()
+        {
+            StringBuilder result_str = new StringBuilder();
+            for (int j = 0; j < _tensor.Length; j++)
+            {
+                result_str.Append(_tensor[j] + " ");
+            }
+            result_str.Append("\n");
+            return result_str.ToString();
+        }
+    }
+}

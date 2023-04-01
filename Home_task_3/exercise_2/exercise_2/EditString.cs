17596e2c1d782fbb23d6f660c14c43bef09ddcb2
Mykhailo <rosmisha@gmail.com>
2023-04-01 19:28:42 +0300

    Add Home_task_3 exercixe_2

---
+++ Home_task_3/exercise_2/exercise_2/EditString.cs
@@ -0,0 +1,121 @@
+﻿using System.Text;
+
+namespace exercise_2
+{
+    internal static class EditString
+    {
+        //The first exercise
+        public static int? FindString(string str_source, string str_found)
+        {
+            //Looking for the first occurrence of the searched string.
+            int first_index = str_source.IndexOf(str_found);
+            if (first_index == -1)
+            {
+                return null;
+            }
+            //Looking to see if there is a second occurrence of the searched string starting from the last occurrence
+            int second_index = str_source.IndexOf(str_found, first_index + 1);
+            if (second_index == -1)
+            {
+                return null;
+            }
+            return second_index;
+        }
+
+        //The second exercise
+        public static int FindUpperCase(string str_source)
+        {
+            int index_out = 0;
+
+            int count = 0;
+            string[] string_array = str_source.Split(' ');
+            int lenght = string_array.Length;
+            
+            while (index_out < lenght)
+            {
+                int index_in = 0;
+                string current = string_array[index_out];
+                //If the current line is empty, then skip it
+                if (current.Length == 0)
+                {
+                    index_out++;
+                    continue;
+                }
+                //If the first character is an uppercase letter, then the counter increases and move to the next line
+                if (current[0] > 64 && current[0] < 91)
+                {
+                    count++;
+                    index_out++;
+                    continue;
+                }
+                else
+                {
+                    while (current[index_in] != '\0')
+                    {
+                        //If the character is a lowercase letter, exit the loop
+                        if ((current[index_in] > 96 && current[index_in] < 123) || (current[index_in] > 47 && current[index_in] < 58))
+                        {
+                            break;
+                        }
+                        //If the character is a number, counter increases and exit the loop
+                        if (current[index_in] > 64 && current[index_in] < 91)
+                        {
+                            count++;
+                            break;
+                        }
+                        index_in++;
+                    }
+                }
+                index_out++;
+            }  
+            return count;
+        }
+
+        //The third exercise
+        public static string ReplaceDouble(string str_source, string str_replace)
+        {
+            string[] string_array = str_source.Split(' ');
+            StringBuilder result_str = new StringBuilder();
+            int iterator = 0;
+            int lenght = string_array.Length;
+
+            while (iterator < lenght)
+            {
+                int iterator_in = 1;
+                string current = string_array[iterator];
+                int current_lenght = current.Length;
+                //If the current line is shorter than two characters, do not check it and move on to the next line
+                if (current_lenght < 2)
+                {
+                    //If the current line is empty, put space in it
+                    if (current == "")
+                    {
+                        string_array[iterator] = " ";
+                    }
+                    result_str.Append(string_array[iterator]);
+                    iterator++;
+                }
+                else
+                {
+                    while (iterator_in < current_lenght)
+                    {
+                        //Сheck whether the current character in the line is a letter
+                        if ((current[iterator_in] > 96 && current[iterator_in] < 123) || (current[iterator_in] > 64 && current[iterator_in] < 91))
+                        {
+                            //If two adjacent characters are the same, then we replace the current line with the one specified by the user
+                            if ((current[iterator_in] == current[iterator_in - 1]))
+                            {
+                                string_array[iterator] = str_replace;
+                                break;
+                            }
+                        }
+                        iterator_in++;
+                    }
+                }
+                result_str.Append(string_array[iterator]+" ");
+                iterator++;
+            }
+            return result_str.ToString();
+        }
+    }
+}
---
+++ Home_task_3/exercise_2/exercise_2/Validator.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+namespace exercise_2
+{
+    internal static class Validator
+    {
+        public static (string str_source, string str_found) InputSearched()
+        {
+            bool marker = true;
+            (bool, string) result;
+            string source = "";
+            string found = "";
+            Console.WriteLine("Hello, World! Enter the values in sequence: Source string, Searched string");
+            while (marker)
+            {
+                Console.Write("Source: ");
+                source = Console.ReadLine();
+                Console.Write("Searched: ");
+                found = Console.ReadLine();
+                result = IsValidSearched(source, found);
+                if (result.Item1) marker = false;
+                else marker = true;
+                Console.WriteLine(result.Item2);
+            }
+
+            return (source, found);
+        }
+        public static (bool, string) IsValidSearched(string str_source, string str_found)
+        {
+            bool marker = true;
+            StringBuilder sb = new StringBuilder();
+            if (str_found.Length > str_source.Length)
+            {
+                String warning = "WARNING! Searched string can't be bigger than Source string";
+                sb.AppendLine(warning);
+                marker = false;
+            }
+            if (str_found.Length == 0)
+            {
+                String warning = "WARNING! Searched string can't be empty";
+                sb.AppendLine(warning);
+                marker = false;
+            }
+            if (str_source.Length == 0)
+            {
+                String warning = "WARNING! Source string can't be empty";
+                sb.AppendLine(warning);
+                marker = false;
+            }
+            if (marker)
+            {
+                sb.AppendLine("All input values are correct");
+            }
+            return (marker, sb.ToString());
+        }
+    }
+}
+

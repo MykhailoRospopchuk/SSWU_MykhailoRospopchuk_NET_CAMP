b1764823830976201cd34cb1b9464e958ce9f0de
Mykhailo <rosmisha@gmail.com>
2023-04-18 15:17:50 +0300

    Work on Home_task_5 exercise_2

---
+++ Home_task_5/exercise_2/exercise_2/Item.cs
@@ -0,0 +1,186 @@
+﻿
+using System.Text;
+
+namespace exercise_2
+{
+    internal class Item
+    {
+        private bool _box;
+        private bool _product_inside;
+        private int _lenght;
+        private int _heigth;
+        private int _width;
+        private List<Item> _items = new List<Item>();
+        private string _title;
+
+        public Item(bool box, string title, int lenght = 0, int heigth = 0, int width = 0)
+        {
+            _box = box;
+            _lenght = lenght;
+            _heigth = heigth;
+            _width = width;
+            _title = title;
+        }
+
+        public void CreatePath(string[] path, int i)
+        {
+            Item temp = null;
+            if (i < path.Length)
+            {
+                if (!_items.Any(ite => ite.Title == path[i]))
+                {
+                    _items.Add(new Item(true, path[i]));
+                    temp = _items.LastOrDefault();
+                }
+                else
+                {
+                    temp = _items.Find(ite => ite.Title == path[i]);
+                }
+                
+                temp.CreatePath(path, ++i);
+            }
+        }
+
+        public Item SetItemInDepart(string depart, Item income_item)
+        {
+            if (depart != "")
+            {
+                foreach (Item item in _items)
+                {
+                    if (item.Title == depart)
+                    {
+                        Console.WriteLine($"{depart} found");
+                        item.PutItem(income_item);
+                        return item;
+                    }
+                }
+                foreach (Item item in _items)
+                {
+                    if (item._items.Count() != 0)
+                    {
+                        var result = item.SetItemInDepart(depart, income_item);
+                        if (result != null)
+                        {
+                            return item;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        public bool PutItem(Item item)
+        {
+            if (!_box)
+            {
+                return false;
+            }
+
+            if (_items.Count() == 0)
+            {
+                _items.Add(item);
+                _product_inside = !item.IsBox;
+                return true;
+            }
+            else
+            {
+                if (item.IsBox && !_product_inside)
+                {
+                    _items.Add(item);
+                    _product_inside = !item.IsBox;
+                    return true;
+                }
+                else if (!item.IsBox && _product_inside)
+                {
+                    _items.Add(item);
+                    _product_inside = !item.IsBox;
+                    return true;
+                }
+            }
+            return false;
+        }
+        public List<Item> GetItems() { return _items; }
+
+        public void CountMetrix()
+        {
+            CountHeight();
+            CountWidth();
+            CountLenght();
+        }
+        public int CountHeight()
+        {
+
+            if (_items.Count != 0)
+            {
+                _items.ForEach(item =>
+                {
+                    item.CountHeight();
+                    
+                });
+                _heigth = _items.Sum(item => item.Heigth);
+            }
+            return _heigth;
+        }
+        public int CountWidth()
+        {
+            if (_items.Count != 0)
+            {
+                _items.ForEach(item =>
+                {
+                    item.CountWidth();
+                });
+                _width = _items.Max(item => item.Width);
+            }
+            
+            return _width;
+        }
+        public int CountLenght()
+        {
+
+            if (_items.Count != 0)
+            {
+                _items.ForEach(item => item.CountLenght());
+                _lenght = _items.Max(item => item.Lenght);
+            }
+            return _lenght;
+        }
+        public bool InsideCommodity
+        { get { return _product_inside; } }
+
+        public bool IsBox
+        { get { return _box; } }
+
+        public int Lenght
+        {
+            get { return _lenght; }
+            set { _lenght = value; }
+        }
+        public int Heigth
+        {
+            get { return _heigth; }
+            set { _heigth = value; }
+        }
+        public int Width
+        {
+            get { return _width; }
+            set { _width = value; }
+        }
+        public string Title
+        {
+            get { return _title; } 
+            set { _title = value; }
+        }
+
+        public string? Print(int i)
+        {
+            i += 1;
+            CountMetrix();
+            StringBuilder sb = new StringBuilder();
+            string title = _box ? $"Department: {_title}" : $"Product: {_title}";
+            sb.Append(title);
+            sb.Append($"\tHeight: {_heigth}; Width: {_width}; Lenght: {_lenght}\n");
+            _items.ForEach(item => sb.Append($"{new string('\t', i)}" + item.Print(i)));
+            return sb.ToString();
+        }
+    }
+}
---
+++ Home_task_5/exercise_2/exercise_2/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace exercise_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Item Silpo = new Item(true, "Silpo");
+            
+            Item meat = new Item(true, "Meat");
+            Item vegatebles = new Item(true, "Vegatebles");
+            Item onion = new Item(false, "Onion", 50, 60, 180);
+            Item pork = new Item(false, "Pork", 150, 460, 150);
+            Item beef = new Item(false, "Beef", 100, 166, 205);
+            vegatebles.PutItem(onion);
+
+            meat.PutItem(pork);
+            meat.PutItem(beef);
+
+            Silpo.PutItem(meat);
+            Silpo.PutItem(vegatebles);
+
+            Item household = new Item(true, "Household");
+            Item detergents = new Item(true, "Detergents");
+            Item organic_detergents = new Item(true, "Organic_Detergents");
+            Item tite = new Item(false, "Tite", 450, 156, 77);
+
+            Item gala = new Item(false, "Gala", 350, 186, 95);
+
+
+            organic_detergents.PutItem(tite);
+            organic_detergents.PutItem(gala);
+            detergents.PutItem(organic_detergents);
+            detergents.PutItem(gala);
+            household.PutItem(detergents);
+            Silpo.PutItem(household);
+
+            Console.WriteLine(Silpo.Print(0));
+
+            
+            var res = Silpo.SetItemInDepart("Organic_Detergents", new Item(false, "Vanish", 128, 230, 50));
+
+            Console.WriteLine(res.Print(0));
+
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine(Silpo.Print(0));
+
+
+            Console.WriteLine("Hello, World! ATB");
+            Item ATB = new Item(true, "ATB");
+            string custom_path = "Household/Detergents/Organic_Detergents";
+            ATB.CreatePath(custom_path.Split('/'), 0);
+            Console.WriteLine(ATB.Print(0));
+            string custom_path2 = "Household/Detergents/Synthetic_Detergents";
+            ATB.CreatePath(custom_path2.Split('/'), 0);
+            Console.WriteLine(ATB.Print(0));
+
+            ATB.SetItemInDepart("Synthetic_Detergents", new Item(false, "Vanish_synt", 128, 230, 50));
+            ATB.SetItemInDepart("Organic_Detergents", new Item(false, "Vanish", 128, 230, 50));
+            Console.WriteLine(ATB.Print(0));
+        }
+    }
+}
